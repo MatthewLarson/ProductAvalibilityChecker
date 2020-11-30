92efffeffd79f86aedf366fd30c1ed868d78a5ef
Matt Larson <larson.matt.b@gmail.com>
2020-11-30 10:06:26 -0600

    Making a version with drop-down selection of sites.

---
+++ ProductAvalibilityChecker/Objects.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PAC.Objects
+{
+    class Site
+    {
+        public string url { get; set; }
+        public string selector { get; set; }
+        public string compareValue { get; set; }
+    }
+}
--- ProductAvalibilityChecker/Scraper.cs
+++ ProductAvalibilityChecker/Scraper.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
+using PAC.Objects;
 
 namespace PAC.Scraper
 {
@@ -17,11 +18,13 @@ namespace PAC.Scraper
         private HtmlNode _doc = null;
         private HtmlNode _node = null;
         public int itemAvailable = -1;
+        public Site site = null;
 
         public ProductScraper(string url)
         {
             _url = url;
             _web = new HtmlWeb();
+            _web.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.66 Safari/537.36";
         }
 
         public async Task<int> CheckForContentMatch(string selector, string compareValue)
@@ -55,6 +58,21 @@ namespace PAC.Scraper
         private async Task GetElement()
         {
             _node = _doc.QuerySelector(_selector);
+        }
+
+        private Site getSite(int id)
+        {
+            switch (id)
+            {
+                case 1:
+                    return new Site { url = "https://www.xbox.com/en-us/configure/8wj714n3rbtl", selector = ".src-pages-BundleBuilder-components-BundleBuilderHeader-__BundleBuilderHeader-module___checkoutButton", compareValue = "Out of stock" };
+                case 2:
+                    return new Site { url = "https://www.xbox.com/en-us/configure/8wj714n3rbtl", selector = ".src-pages-BundleBuilder-components-BundleBuilderHeader-__BundleBuilderHeader-module___checkoutButton", compareValue = "Out of stock" };
+                case 3:
+                    return new Site { url = "https://www.xbox.com/en-us/configure/8wj714n3rbtl", selector = ".src-pages-BundleBuilder-components-BundleBuilderHeader-__BundleBuilderHeader-module___checkoutButton", compareValue = "Out of stock" };
+                default:
+                    return null;
+            }
         }
         
     }
